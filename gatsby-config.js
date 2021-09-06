8b36fedc63cc88a551b18a70ca183768229db638
hisamatsu yoshiyuki <hisasann25@gmail.com>
2021-09-06 23:29:02 +0900

    feat: add chakra-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,6 +44,7 @@ module.exports = {
         },
       },
     },
+    '@chakra-ui/gatsby-plugin',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
