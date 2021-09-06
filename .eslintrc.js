7fe7277410a815373d2b14ce941612a8594c39fa
hisamatsu yoshiyuki <hisasann25@gmail.com>
2021-09-06 21:37:45 +0900

    chore(eslint): add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  ignorePatterns: ['public'],
+  rules: {
+    '@typescript-eslint/ban-ts-comment': 'off',
+  },
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,47 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://sour-lemon.dev`,
-    title: `Sour & Lemon Inc.🍋`,
-    description: `Take each other's hands in joy.`,
-    author: `@SourLemonInc`,
+    siteUrl: 'https://sour-lemon.dev',
+    title: 'Sour & Lemon Inc.🍋',
+    description: "Take each other's hands in joy.",
+    author: '@SourLemonInc',
   },
   // via https://github.com/gatsbyjs/gatsby/discussions/29308
   flags: {
     DEV_SSR: false,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/logo/icon-256x256.png`, // This path is relative to the root of the site.
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/logo/icon-256x256.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-eslint',
+      options: {
+        test: /\.js$|\.jsx$|\.ts$|\.tsx$/,
+        exclude: /(node_modules|.cache|public)/,
+        stages: ['develop'],
+        options: {
+          emitWarning: true,
+          failOnError: false,
+        },
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
