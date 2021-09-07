2306e97fbe7949eb1a12f7ebd2b63ae2791d3a48
hisamatsu yoshiyuki <hisasann25@gmail.com>
2021-09-07 13:42:17 +0900

    feat: change links in footer

--- src/components/largeWithNewsletter.tsx
+++ src/components/largeWithNewsletter.tsx
@@ -75,7 +75,7 @@ export default function LargeWithNewsletter() {
     >
       <Container as={Stack} maxW={'6xl'} py={10}>
         <SimpleGrid
-          templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr 2fr' }}
+          templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr 1fr' }}
           spacing={8}
         >
           <Stack spacing={6}>
@@ -112,48 +112,31 @@ export default function LargeWithNewsletter() {
             <ListHeader>Company</ListHeader>
             <Link
               href={
-                'https://docs.google.com/forms/d/1YOTuc6H0dknQjoKRghL0j4Axo8c4lkuRYJmMPF21nEI/edit'
+                'https://forms.gle/exFMWFtdb9msd84f7'
               }
-            >
-              About us
-            </Link>
-            <Link href={'#'}>Blog</Link>
-            <Link
-              href={
-                'https://docs.google.com/forms/d/1YOTuc6H0dknQjoKRghL0j4Axo8c4lkuRYJmMPF21nEI/edit'
-              }
+              isExternal
             >
               Contact us
             </Link>
           </Stack>
           <Stack align={'flex-start'}>
             <ListHeader>Support</ListHeader>
-            <Link href={'#'}>Terms of Service</Link>
-            <Link href={'#'}>Legal</Link>
-            <Link href={'#'}>Privacy Policy</Link>
-            <Link href={'#'}>Satus</Link>
+            <Link href={'https://forms.gle/W8tosBmVMdHm3SoM9'}>お便り</Link>
           </Stack>
           <Stack align={'flex-start'}>
-            <ListHeader>Stay up to date</ListHeader>
-            <Stack direction={'row'}>
-              <Input
-                placeholder={'Your email address'}
-                bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-                border={0}
-                _focus={{
-                  bg: 'whiteAlpha.300',
-                }}
-              />
-              <IconButton
-                bg={useColorModeValue('green.400', 'green.800')}
-                color={useColorModeValue('white', 'gray.800')}
-                _hover={{
-                  bg: 'green.600',
-                }}
-                aria-label="Subscribe"
-                icon={<BiMailSend />}
-              />
-            </Stack>
+            <ListHeader>Follow Us</ListHeader>
+            <Link
+              href={'https://github.com/SourLemonInc'}
+              isExternal
+            >Github</Link>
+            <Link
+              href={'https://twitter.com/sourlemoninc'}
+              isExternal
+            >Twitter</Link>
+            <Link
+              href={'https://www.facebook.com/SourLemonInc/?ref=pages_you_manage'}
+              isExternal
+            >Facebook</Link>
           </Stack>
         </SimpleGrid>
       </Container>
